c5edf4165ccc86e49f5f620df5e44602eac22289
UhaiMong <uhaimarma2016@gmail.com>
2023-10-11 01:38:50 +0600

    2.Requirement done

--- hall_management.py
+++ hall_management.py
@@ -1,5 +1,23 @@
 class Star_Cinema:
-    hall_list = []
+    __hall_list = []
 
     def entry_hall(self, hall):
-        self.hall_list.append(hall)
+        Star_Cinema.__hall_list.append(hall)
+
+
+class Hall(Star_Cinema):
+    def __init__(self, rows, cols, hall_no) -> None:
+        self._seats = {}
+        self._show_list = []
+        self._rows = rows
+        self._cols = cols
+        self._hall_no = hall_no
+        self._initialize_seats()
+        self.entry_hall(self)
+
+    def _initialize_seats(self):
+        for row in range(1, self._rows+1):
+            self._seats[row] = [0] * self._cols
+
+    def entry_show(self, id, movie_name, time):
+        show_info = ()
